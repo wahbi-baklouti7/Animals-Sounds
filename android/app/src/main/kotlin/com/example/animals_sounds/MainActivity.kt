bae367e46e4dba11182a806414ba4dcd898982b6
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-04-18 13:16:07 +0100

    upload project folder

---
+++ android/app/src/main/kotlin/com/example/animals_sounds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animals_sounds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[400],
+        appBar: AppBar(
+      title: Text("Animals Sounds"),
+      centerTitle: true,
+      backgroundColor: Colors.amber[600],
+    ));
+  }
+}
