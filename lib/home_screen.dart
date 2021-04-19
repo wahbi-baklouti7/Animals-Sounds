3cebd9ded9f7df3d921b7423be84e5e0a384046d
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-04-19 10:09:01 +0100

    add functionality into app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +7,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  
+  Card animalCard(String animalName) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      color: Colors.white,
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        FlatButton(
+          onPressed: () {
+            animalSound(animalName);
+          },
+          child: Image.asset(
+            "assets/images/$animalName.png",
+            height: 110,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          animalName,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.3),
+        )
+      ]),
+    );
+  }
+
+  void animalSound(String animalName) {
+    final player = AssetsAudioPlayer();
+    player.open(
+      Audio("assets/sounds/$animalName.mp3"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +54,20 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Colors.amber[600],
         ),
         body: GridView.count(
+          primary: true,
           crossAxisCount: 2,
           crossAxisSpacing: 5,
           mainAxisSpacing: 5,
+          padding: EdgeInsets.all(10),
           children: [
-            Card(shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-              color: Colors.red,),
-            Card(color: Colors.purple,)
+            animalCard("Elephant"),
+            animalCard("Hyena"),
+            animalCard("Monkey"),
+            animalCard("Pig"),
+            animalCard("Penguin"),
+            animalCard("Whale"),
+            animalCard("Wolf"),
+            animalCard("Panther"),
           ],
         ));
   }
